878abd585c009ee80c32a1b0c83df89c3a41754b
mkiuser <makisi@gmx.de>
2015-04-18 16:18:25 +0200

    Cleaning

--- src/com/project/crawler/model/Person.java
+++ src/com/project/crawler/model/Person.java
@@ -1,7 +1,46 @@
 package com.project.crawler.model;
 
+import org.hibernate.annotations.DynamicUpdate;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  * Created by marcel on 15.04.2015.
  */
+@Entity
+@Table
 public class Person {
+    @Id
+    @GeneratedValue(generator = "increment")
+    @GenericGenerator(name = "increment" , strategy = "increment")
+    private int id;
+    private String name;
+    public Person() {}
+    public Person(String name) {
+        setName(name);
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    @Override
+    public String toString(){
+        return "Person[id:"+id+", name:"+name + "]";
+    }
+
 }
--- src/com/project/crawler/model/Stream.java
+++ src/com/project/crawler/model/Stream.java
@@ -1,7 +1,50 @@
 package com.project.crawler.model;
 
+import org.hibernate.annotations.DynamicUpdate;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  * Created by marcel on 15.04.2015.
  */
+@DynamicUpdate
+@Table
 public class Stream {
+    @Id
+    @GeneratedValue(generator = "increment")
+    @GenericGenerator(name = "increment" , strategy = "increment")
+    private int id;
+    private String source;
+    private String url;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+    @Override
+    public String toString(){
+        return "Stream[id:"+id+", source:"+source +", url:"+url + "]";
+    }
 }
