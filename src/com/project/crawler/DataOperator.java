2b66df9c76effb4e43a8bc26b8d19e93680e5000
mkiuser <makisi@gmx.de>
2015-09-06 22:06:16 +0200

    DataOperator

---
+++ src/com/project/crawler/DataOperator.java
@@ -0,0 +1,17 @@
+package com.project.crawler;
+
+/**
+ * Created by Marcel Kisilowski on 06.09.15.
+ */
+public class DataOperator {
+    private DataCrawler dataCrawler;
+    private DataParser dataParser;
+
+    public DataParser getDataParser() {
+        return dataParser;
+    }
+
+    public DataCrawler getDataCrawler() {
+        return dataCrawler;
+    }
+}
