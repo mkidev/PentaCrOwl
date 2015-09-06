5582fbacc590db62c1d9f09ecf1be3066b88db63
mkiuser <makisi@gmx.de>
2015-09-06 20:31:59 +0200

    Game Model

--- src/com/project/model/User.java
+++ src/com/project/model/User.java
@@ -1,6 +1,5 @@
 package com.project.model;
 
-import org.hibernate.annotations.DynamicUpdate;
 import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.Entity;
@@ -13,22 +12,22 @@ import javax.persistence.Table;
  */
 @Entity
 @Table
-public class Person {
+public class User {
     @Id
     @GeneratedValue(generator = "increment")
     @GenericGenerator(name = "increment" , strategy = "increment")
-    private int id;
+    private int userID;
     private String name;
-    public Person() {}
-    public Person(String name) {
+    public User() {}
+    public User(String name) {
         setName(name);
     }
-    public int getId() {
-        return id;
+    public int getUserID() {
+        return userID;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public void setUserID(int userID) {
+        this.userID = userID;
     }
 
     public String getName() {
@@ -40,6 +39,6 @@ public class Person {
     }
     @Override
     public String toString(){
-        return "Person[id:"+id+", name:"+name + "]";
+        return "Person[id:"+ userID +", name:"+name + "]";
     }
 }
