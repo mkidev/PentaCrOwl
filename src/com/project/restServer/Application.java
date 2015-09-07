9d7567b62b311421f410ec5b311607421f6eb34f
mkiuser <makisi@gmx.de>
2015-09-07 21:42:43 +0200

    Restfull

---
+++ src/com/project/restServer/Application.java
@@ -0,0 +1,18 @@
+package com.project.restServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by Marcel Kisilowski on 07.09.15.
+ */
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/com/project/restServer/GameController.java
@@ -0,0 +1,11 @@
+package com.project.restServer;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Marcel Kisilowski on 07.09.15.
+ */
+@RestController
+public class GameController {
+
+}
