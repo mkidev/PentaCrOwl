6f520f6acaa1fb61af4b7d1cd4825336689aa2c1
mkiuser <makisi@gmx.de>
2015-09-07 14:57:59 +0200

    Changed gameID type from int to long

--- src/com/project/model/Game.java
+++ src/com/project/model/Game.java
@@ -17,12 +17,22 @@ public class Game {
     @Id
     @GeneratedValue(generator = "increment")
     @GenericGenerator(name = "increment" , strategy = "increment")
-    private int gameID;
+    private long gameID;
     @NaturalId
     private final String name;
     private final int viewers;
 
+    public Game() {
+        name="";
+        viewers=0;
+    }
+
     public Game(String name, int viewers) {
+        this.name = name;
+        this.viewers = viewers;
+    }
+    public Game(String name, int viewers,long gameID) {
+        this.gameID = gameID;
         this.name = name;
         this.viewers = viewers;
     }
@@ -33,5 +43,29 @@ public class Game {
 
     public int getViewers() {
         return viewers;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Game)) return false;
+
+        Game game = (Game) o;
+
+        return name.equals(game.name);
+
+    }
+
+    @Override
+    public String toString() {
+        return "Game{" +
+                "name='" + name + '\'' +
+                ", viewers=" + viewers +
+                '}';
+    }
+
+    @Override
+    public int hashCode() {
+        return name.hashCode();
     }
 }
